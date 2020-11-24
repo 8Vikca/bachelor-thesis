71d83f8aceaa331026c0fc09407d9b6c63c7c35e
Viktória Matušicová <xmatus32@vutbr.cz>
2020-11-24 15:51:51 +0100

    table created

--- semestralna-praca/src/app/sections/table/table.component.ts
+++ semestralna-praca/src/app/sections/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Attack } from '../../../shared/attack';
 
 @Component({
   selector: 'app-table',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class TableComponent implements OnInit {
 
   constructor() { }
+
+  attacks : Attack[] = [
+    {id:1 , message: 'stp'},
+    {id:2 , message: 'udp'},
+    {id:3 , message: 'mitm'},
+  ];
 
   ngOnInit(): void {
   }
