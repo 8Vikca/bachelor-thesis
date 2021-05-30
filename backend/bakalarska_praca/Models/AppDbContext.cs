3ee39cbf3f7e8f0c2b7df47c366cf768a1991796
xmatus32 <xmatus32@vutbr.cz>
2021-05-30 23:38:23 +0200

    database update

--- backend/bakalarska_praca/Models/AppDbContext.cs
+++ backend/bakalarska_praca/Models/AppDbContext.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace bakalarska_praca.Models
 {
-    public class AppDbContext : DbContext       //praca s databazami
+    public class AppDbContext : DbContext      
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //praca s databazou
         {
